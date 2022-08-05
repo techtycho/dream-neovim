470fad397f92167e85e0047c3e80100d1bb474c4
techtycho <omarahmedabdelhakim@gmail.com>
2022-08-05 23:42:10 +0200

    Create a basic statusline that displays the current mode

--- lua/core/ui/statusline/init.lua
+++ lua/core/ui/statusline/init.lua
@@ -1,1 +1,29 @@
+local M = {}
+
 require "core.ui.statusline.modules"
+
+function M.getMode(mode)
+  if mode == "n" then
+    return "NORMAL"
+  else
+    return "INSERT"
+  end
+end
+
+function M.statusline()
+  return M.getMode(vim.api.nvim_get_mode().mode)
+end
+
+---- Old way of doing things ----
+-- vim.api.nvim_exec([[
+-- function! StatusLine()
+--   let l:mode = mode()
+--   let l:out = luaeval('require("core.ui.statusline").getMode(_A)', l:mode)
+--   return l:out
+-- endfunction
+-- set statusline=%{StatusLine()}
+-- ]], true)
+
+vim.opt.statusline = "%!v:lua.require('core.ui.statusline').hello()"
+
+return M
