84b93bb886bbd927dae35c10525b940eb9c6a88b
techtycho <omarahmedabdelhakim@gmail.com>
2022-08-18 13:34:06 +0200

    Configure dashboard and fix bufferline

--- lua/core/ui/dashboard.lua
+++ lua/core/ui/dashboard.lua
@@ -1,33 +1,48 @@
-local home = os.getenv('HOME')
-local db = require('dashboard')
--- linux
---[[ db.preview_command = 'ueberzug' ]]
---[[ db.preview_file_path = home .. '/.config/nvim//neovim.cat' ]]
+local home = os.getenv("HOME")
+local db = require("dashboard")
+
+db.custom_header = {
+  "",
+  "",
+  "",
+  "",
+  " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+  " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+  " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+  " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+  " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+  " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+  "",
+  "Better than VSCode BTW",
+  "",
+  "",
+}
+
 db.preview_file_height = 12
 db.preview_file_width = 80
 db.custom_center = {
-    {icon = '  ',
-    desc = 'Recently latest session                  ',
-    shortcut = 'SPC s l',
-    action ='SessionLoad'},
-    {icon = '  ',
-    desc = 'Recently opened files                   ',
-    action =  'DashboardFindHistory',
-    shortcut = 'SPC f h'},
-    {icon = '  ',
-    desc = 'Find  File                              ',
-    action = 'Telescope find_files find_command=rg,--hidden,--files',
-    shortcut = 'SPC f f'},
-    {icon = '  ',
-    desc ='File Browser                            ',
-    action =  'Telescope file_browser',
-    shortcut = 'SPC f b'},
-    {icon = '  ',
-    desc = 'Find  word                              ',
-    action = 'Telescope live_grep',
-    shortcut = 'SPC f w'},
-    {icon = '  ',
-    desc = 'Open Personal dotfiles                  ',
-    action = 'Telescope dotfiles path=' .. home ..'/.dotfiles',
-    shortcut = 'SPC f d'},
-  }
+  { icon = "  ",
+  desc = "Recently latest session                 ",
+  shortcut = "SPC t l",
+  action ="SessionLoad"},
+  {icon = "  ",
+  desc = "New File                                ",
+  action =  "DashboardNewFile",
+  shortcut = "SPC t h"},
+  {icon = "  ",
+  desc = "Find File                               ",
+  action = "Telescope find_files find_command=rg,--hidden,--files",
+  shortcut = "SPC t f"},
+  {icon = "  ",
+  desc ="Astronomy                               ",
+  action =  "Telescope planets",
+  shortcut = "SPC t b"},
+  {icon = "  ",
+  desc = "Find  word                              ",
+  action = "Telescope live_grep",
+  shortcut = "SPC t w"},
+  {icon = "  ",
+  desc = "Open Personal dotfiles                  ",
+  action = "Telescope dotfiles path=" .. home .."/.dotfiles",
+  shortcut = "SPC t d"},
+}
