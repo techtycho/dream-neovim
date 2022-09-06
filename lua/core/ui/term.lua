703c0f4553f47b390ced0b399fddd51f699643e4
techtycho <omarahmedabdelhakim@gmail.com>
2022-09-06 13:03:13 +0200

    ignore useless testing files

--- lua/core/ui/term.lua
+++
@@ -1,8 +0,0 @@
-function _G.open_term()
-  vim.cmd "botright new +term"
-  vim.wo.number = false
-  vim.cmd "startinsert!"
-  vim.cmd "tnoremap <Esc><Esc> <C-\\><C-n>:q!<CR>"
-end
-
-vim.cmd "nnoremap <leader>tre :lua _G.open_term()<CR>"
